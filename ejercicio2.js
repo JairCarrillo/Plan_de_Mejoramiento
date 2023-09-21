bcafe96cec57ec625b6556291ff2d7803b3c9b5e
JairCarrillo <113036574+JairCarrillo@users.noreply.github.com>
2023-09-21 11:22:15 -0500

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,22 @@
+const peso = prompt("Ingresa tu peso");
+const estatura = prompt("Ingresa tu altura");
+;
+console.log("el resultado es :" + resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ejercicio4.js
@@ -0,0 +1,1 @@
+console.log("El señor rafael comrpro")
